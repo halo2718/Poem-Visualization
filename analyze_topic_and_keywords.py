243d36ae5d44ae7ea69a633f4fce2540899ef764
Phosphor-Bai <byz22@mails.tsinghua.edu.cn>
2023-06-11 17:34:11 +0800

    Finished data preprocess(only emotion detection left).

--- analyze_topic_and_keywords.py
+++ analyze_topic_and_keywords.py
@@ -2,6 +2,7 @@ import os
 import sys
 import math
 import json
+import itertools
 import random
 import numpy as np
 import pandas as pd
@@ -93,6 +94,76 @@ def get_topic_data(topic_k=100):
     )
     print(prepared_data.keys())
 
-analyze_topic_for_dynasty()
+def dynasty_keywords_analysis():
+    keywords = ['花', '云', '春', '日', '月', '山', '风', '君', '天', '水', '雨', '空', '心', '生', '老', '梦', '客', '寒', '秋', '飞', '犹', '行', '夜', '莫', '新', '深', '酒', '愁', '清', '满', '难', '声', '香', '落', '家', '独', '重', '树', '远', '处', '问', '烟', '诗', '闻', '高', '雪', '醉', '道', '共', '书', '思', '笑', '旧', '路', '初', '留', '吹', '坐', '红', '真']
+    dynasty_freq_dict = {}
+    for txt_file in os.listdir('dataset/segmented'):
+        freq_dict = {k:0 for k in keywords}
+        document_num = 0
+        for line in tqdm(open(os.path.join('dataset/segmented', txt_file)).readlines()):
+            document_num += 1
+            line = line.strip().split()
+            line = [w for w in line if '□' not in w]
+            counter = Counter(line)
+            for k in keywords:
+                freq_dict[k] += counter[k]
+        freq_dict = {k:((freq_dict[k]+1)/document_num) for k in keywords}
+        dynasty_freq_dict[txt_file[:-4]] = freq_dict
+    with open('data/keywords_change_by_dynasty.json', 'w', encoding='utf-8') as f:
+        json.dump(dynasty_freq_dict, f, ensure_ascii=False, indent=4)
+
+def topic_keywords_analysis(topic_k=100):
+    keywords = ['花', '云', '春', '日', '月', '山', '风', '君', '天', '水', '雨', '空', '心', '生', '老', '梦', '客', '寒', '秋', '飞', '犹', '行', '夜', '莫', '新', '深', '酒', '愁', '清', '满', '难', '声', '香', '落', '家', '独', '重', '树', '远', '处', '问', '烟', '诗', '闻', '高', '雪', '醉', '道', '共', '书', '思', '笑', '旧', '路', '初', '留', '吹', '坐', '红', '真']
+    res_dists = {}
+    model = tp.LDAModel.load(f'model/topic_model/lda_{topic_k}.bin')
+    vocab = list(model.used_vocabs)
+    keyword_ids = [vocab.index(w) for w in keywords]
+    for i in range(topic_k):
+        topic_word_dist = model.get_topic_word_dist(topic_id=i)
+        topic_keyword_dist = topic_word_dist[keyword_ids]
+        topic_keyword_dist = topic_keyword_dist / topic_keyword_dist.sum()
+        res_dists[i] = {keywords[idx]:np.float64(topic_keyword_dist[idx]) for idx in range(len(keywords))}
+
+    with open('data/topic_keywords_chords.json', 'w', encoding='utf-8') as f:
+        json.dump(res_dists, f, ensure_ascii=False, indent=4)
+
+def keywords_analysis():
+    keywords = ['花', '云', '春', '日', '月', '山', '风', '君', '天', '水', '雨', '空', '心', '生', '老', '梦', '客', '寒', '秋', '飞', '犹', '行', '夜', '莫', '新', '深', '酒', '愁', '清', '满', '难', '声', '香', '落', '家', '独', '重', '树', '远', '处', '问', '烟', '诗', '闻', '高', '雪', '醉', '道', '共', '书', '思', '笑', '旧', '路', '初', '留', '吹', '坐', '红', '真']
+    keyword_num = len(keywords)
+    keyword_matrix = np.zeros((keyword_num, keyword_num))
+    for txt_file in os.listdir('dataset/segmented'):
+        document_num = 0
+        for line in tqdm(open(os.path.join('dataset/segmented', txt_file)).readlines()):
+            keyword_set = []
+            document_num += 1
+            line = line.strip().split()
+            line = [w for w in line if '□' not in w]
+            counter = Counter(line)
+            for k in keywords:
+                idx = keywords.index(k)
+                keyword_matrix[idx][idx] += counter[k]
+                if counter[k]:
+                    keyword_set.append(idx)
+            keyword_pairs = list(itertools.combinations(keyword_set, 2))
+            for (i1, i2) in keyword_pairs:
+                keyword_matrix[i1][i2] += 1
+                keyword_matrix[i2][i1] += 1
+    keyword_freq = {keywords[i]:keyword_matrix[i][i] for i in range(keyword_num)}
+    keyword_co_occurance_freq = []
+    for i in range(1, keyword_num):
+        for j in range(i):
+            d = {'keyword1': keywords[i], 'keyword2': keywords[j]}
+            co_occurance = keyword_matrix[i][j] * keyword_matrix[i][j] / (keyword_matrix[i][i]*keyword_matrix[j][j])
+            d['co_occurance'] = co_occurance
+            keyword_co_occurance_freq.append(d)
+    with open('data/keywords_freq.json', 'w', encoding='utf-8') as f:
+        json.dump(keyword_freq, f, ensure_ascii=False, indent=4)
+    with open('data/keywords_co_occurance.json', 'w', encoding='utf-8') as f:
+        json.dump(keyword_co_occurance_freq, f, ensure_ascii=False, indent=4)
+        
+# analyze_topic_for_dynasty()
 # tf_idf()
 # get_topic_data()
+# dynasty_keywords_analysis()
+# topic_keywords_analysis()
+keywords_analysis()
--- readme.md
+++ readme.md
@@ -5,8 +5,18 @@
 对应`preprocess_poem.py`和`lda.py`。
 1. 将json数据提取为csv
 2. 使用[DeepTHULAC](https://github.com/thunlp/DeepTHULAC)对古诗词数据进行分词
-3. 使用[tomotopy](https://bab2min.github.io/tomotopy/v0.12.2/en/)基于分词后的数据学习LDA主题模型
+3. 使用[tomotopy](https://bab2min.github.io/tomotopy/v0.12.2/en/)基于分词后的数据学习LDA主题模型，主题数目设为100
 	* 由于不同朝代诗词数量差距较大（例如明代诗词有26余万首，而大部分朝代的诗词数量都在几千、几万首量级），为了避免某个年代对主题有过大的影响，我们对于存在超过6万首诗词的朝代随机抽取其中的6万首诗词
 4. 使用学到的LDA主题模型对每首诗词进行分析，获得其对应的top-3主题及对应概率
 ### 意象抽取
-按词频？
+使用tf-idf进行抽取，并人工判断了是否为意象，最终保留前60个。
+
+### 气泡图、河流图数据预处理
+对于每个朝代，将该朝代所有诗词的top-3主题概率相加，平均后得到100个主题在该朝代的平均概率分布，取前20个用于绘制气泡图，存储于`data/bubble.json`。河流图同理。
+### 意象排序变化图
+对于关键词统计在各个朝代对应诗词中的词频，存储于`data/keywords_change_by_dynasty.json`。
+TODO：词频情感分析（大概找个情感分类BERT，每次这个词出现的时候对对应诗词做一下情感分析，是正向还是负向有个得分，然后最后对每个朝代取一下情感得分的均分就行吧）
+### 主题-意象弦图
+使用LDA模型计算每个主题的词分布，提取出60个意象对应的词分布，并进行正则化，存储于data/`topic_keywords_chords.json`。
+### 意象关联弦图
+计算意象的本身频度及共现次数，分别存储于`data/keywords_freq.json`和`data/keywords_co_occurance.json`。
